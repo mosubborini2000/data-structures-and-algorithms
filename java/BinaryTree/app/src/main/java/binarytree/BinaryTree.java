c17649d585b609196148213415938d9b971d511c
mosubborini2000 <mosubborini2000@gmail.com>
2023-09-16 14:27:22 +0000

    cc15

---
+++ java/BinaryTree/app/src/main/java/binarytree/BinaryTree.java
@@ -0,0 +1,53 @@
+package binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+
+    public List<Integer> preOrder() {
+        List<Integer> result = new ArrayList<>();
+        preOrderTraversal(root, result);
+        return result;
+    }
+
+    private void preOrderTraversal(Node node, List<Integer> result) {
+        if (node != null) {
+            result.add(node.value);
+            preOrderTraversal(node.left, result);
+            preOrderTraversal(node.right, result);
+        }
+    }
+
+
+    public List<Integer> inOrder() {
+        List<Integer> result = new ArrayList<>();
+        inOrderTraversal(root, result);
+        return result;
+    }
+
+    private void inOrderTraversal(Node node, List<Integer> result) {
+        if (node != null) {
+            inOrderTraversal(node.left, result);
+            result.add(node.value);
+            inOrderTraversal(node.right, result);}
+    }
+    private void postOrderTraversal(Node node, List<Integer> result) {
+        if (node != null) {
+            postOrderTraversal(node.left, result);
+            postOrderTraversal(node.right, result);
+            result.add(node.value);
+        }
+    }
+    public List<Integer> postOrder() {
+        List<Integer> result = new ArrayList<>();
+        postOrderTraversal(root, result);
+        return result;
+    }
+}
